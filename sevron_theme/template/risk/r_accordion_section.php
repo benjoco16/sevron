cf5ccd47e113bb863291f7f30c22580ade70a53d
benjoco10 <benjoco10@gmail.com>
2019-10-27 23:20:56 +0800

    risk assestment

--- sevron_theme/template/risk/r_accordion_section.php
+++ sevron_theme/template/risk/r_accordion_section.php
@@ -24,40 +24,96 @@
                         <a class="ml-auto mr-5 font-weight-500 font-ash">Save & Previous</a>
                         <button class="btn btn-custom-blue btn-wide">Save & Next</button>
                     </div>
-                    
                 </div>
             </div>
         </div>
     </div>
     <!--end::Accordion-->  
 
+    <!--begin::Substance Accordion-->
     <div class="accordion accordion-light  accordion-toggle-arrow" id="accordionExample3">
         <div class="card risk_accord">
             <div class="card-header" id="headingOne3">
                 <div class="card-title collapsed" data-toggle="collapse" data-target="#collapseOne3" aria-expanded="false" aria-controls="collapseOne3">
-                    <span>Not Started</span> Task Hazards and Control
+                    <span>Not Started</span> Substance Information
                 </div>
             </div>
             <div id="collapseOne3" class="collapse" aria-labelledby="headingOne3" data-parent="#accordionExample3">
                 <div class="card-body pl-4 pr-4 pt-1 pb-1">
-
-                    <div class="card inner-accordion-card mt-4">
-                        <div class="card-header font-weight-500 font-ash">
-                        Control icons to go here...
+                    <div class="kt-section si_section">
+                        <div class="kt-section__content">
+                            <table class="table table_si">
+                                <tbody>
+                                    <tr>
+                                        <td><strong> Product Name (Code)</strong></td>
+                                        <td><strong>Manufacturer</strong></td>
+                                        <td><strong>Hazard Symbols</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Sodium (483745)</td>
+                                        <td>Sigma-Aldrich Company Ltd</td>
+                                        <td><img src="assets/media/risk/symbols.png" alt=""></td>
+                                    </tr>
+                                    <tr>
+                                        <td> <a href="#"> Expand <img src="assets/media/risk/zoom_icon.png" alt=""></a></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            
                         </div>
                     </div>
-                    <div class="task-details">
-                        <textarea name="" id="" cols="30" rows="10" placeholder="Please provide a detailed description of the task that is being carried"></textarea>
-                    </div>
-                    <div class="d-flex align-items-center mt-4 mb-4">
-                        <a class="ml-auto mr-5 font-weight-500 font-ash">Save & Previous</a>
-                        <button class="btn btn-custom-blue btn-wide">Save & Next</button>
-                    </div>
-                    
+                    <div class="kt-section si_section">
+					<div class="kt-section__content">
+						<table class="table table_si">
+						  	<tbody>
+						    	<tr>
+							      	<td><strong> Product Name (Code)</strong></td>
+							      	<td><strong>Manufacturer</strong></td>
+							      	<td><strong>Hazard Symbols</strong></td>
+						    	</tr>
+						    	<tr>
+						      		<td>Sodium (483745)</td>
+						      		<td>Sigma-Aldrich Company Ltd</td>
+						      		<td><img src="assets/media/risk/symbols.png" alt=""></td>
+						    	</tr>
+						    	<tr>
+						      		<td> <a href="#"> Expand <img src="assets/media/risk/zoom_icon.png" alt=""></a></td>
+						    	</tr>
+						  	</tbody>
+                        </table>
+                        
+					</div>
+                </div>
+                <div class="d-flex align-items-center mt-4 mb-4">
+                    <a class="ml-auto mr-5 font-weight-500 font-ash">Save &amp; Previous</a>
+                    <button class="btn btn-custom-blue btn-wide">Save &amp; Next</button>
+                </div>
                 </div>
             </div>
         </div>
     </div>
+    <!--end::Substance Accordion-->
+
+    <!--begin::Task Hazards & Controls-->
+    <div class="accordion accordion-light accordion-toggle-arrow mb-4" id="accordionExample2">
+        <div class="card risk_accord">
+            <div class="card-header" id="headingThree2">
+                <div class="card-title collapsed" data-toggle="collapse" data-target="#collapseThree2" aria-expanded="false" aria-controls="collapseOne2">
+                    <span>Not Started</span> Task Hazards & Controls
+                </div>
+            </div>
+            <div id="collapseThree2" class="collapse" aria-labelledby="headingThree2" data-parent="#accordionExample2">
+                <div class="card-body pl-4 pr-4 pt-1 pb-1 haz">
+                   <img src="assets/media/risk/hazard_sect.png" alt="">
+                </div>
+                <div class="d-flex align-items-center mt-4 mb-4">
+                    <a class="ml-auto mr-5 font-weight-500 font-ash">Save &amp; Previous</a>
+                    <button class="btn btn-custom-blue btn-wide">Save &amp; Next</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--end::Task Hazards & Controls-->
 </div>
 
 		<!--end::Portlet-->
