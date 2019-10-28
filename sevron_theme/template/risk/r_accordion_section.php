228449362af23d1dd755620513fd192e34d8d02e
benjoco10 <benjoco10@gmail.com>
2019-10-29 03:19:34 +0800

    risk assesment

--- sevron_theme/template/risk/r_accordion_section.php
+++ sevron_theme/template/risk/r_accordion_section.php
@@ -1,118 +1,17 @@
 <!-- Accordion -->
 <!--begin::Portlet-->
 <div class="kt-portlet-spacer bg-light-gray "> 
-    <!--begin::Accordion-->
-    <div class="accordion accordion-light accordion-toggle-arrow mb-4" id="accordionExample2">
-        <div class="card risk_accord">
-            <div class="card-header" id="headingOne2">
-                <div class="card-title collapsed" data-toggle="collapse" data-target="#collapseOne2" aria-expanded="false" aria-controls="collapseOne2">
-                    <span>Not Started</span> Task Details
-                </div>
-            </div>
-            <div id="collapseOne2" class="collapse" aria-labelledby="headingOne2" data-parent="#accordionExample2">
-                <div class="card-body pl-4 pr-4 pt-1 pb-1">
 
-                    <div class="card inner-accordion-card mt-4">
-                        <div class="card-header font-weight-500 font-ash">
-                        Control icons to go here...
-                        </div>
-                    </div>
-                    <div class="task-details">
-                        <textarea name="" id="" cols="30" rows="10" placeholder="Please provide a detailed description of the task that is being carried"></textarea>
-                    </div>
-                    <div class="d-flex align-items-center mt-4 mb-4">
-                        <a class="ml-auto mr-5 font-weight-500 font-ash">Save & Previous</a>
-                        <button class="btn btn-custom-blue btn-wide">Save & Next</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--end::Accordion-->  
+    <!-- begin::Task Details accordion -->
+    <?php include('accordion-sections/task_details.php') ?>
+    <!-- end::Task details accordion -->
 
-    <!--begin::Substance Accordion-->
-    <div class="accordion accordion-light  accordion-toggle-arrow" id="accordionExample3">
-        <div class="card risk_accord">
-            <div class="card-header" id="headingOne3">
-                <div class="card-title collapsed" data-toggle="collapse" data-target="#collapseOne3" aria-expanded="false" aria-controls="collapseOne3">
-                    <span>Not Started</span> Substance Information
-                </div>
-            </div>
-            <div id="collapseOne3" class="collapse" aria-labelledby="headingOne3" data-parent="#accordionExample3">
-                <div class="card-body pl-4 pr-4 pt-1 pb-1">
-                    <div class="kt-section si_section">
-                        <div class="kt-section__content">
-                            <table class="table table_si">
-                                <tbody>
-                                    <tr>
-                                        <td><strong> Product Name (Code)</strong></td>
-                                        <td><strong>Manufacturer</strong></td>
-                                        <td><strong>Hazard Symbols</strong></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Sodium (483745)</td>
-                                        <td>Sigma-Aldrich Company Ltd</td>
-                                        <td><img src="assets/media/risk/symbols.png" alt=""></td>
-                                    </tr>
-                                    <tr>
-                                        <td> <a href="#"> Expand <img src="assets/media/risk/zoom_icon.png" alt=""></a></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            
-                        </div>
-                    </div>
-                    <div class="kt-section si_section">
-					<div class="kt-section__content">
-						<table class="table table_si">
-						  	<tbody>
-						    	<tr>
-							      	<td><strong> Product Name (Code)</strong></td>
-							      	<td><strong>Manufacturer</strong></td>
-							      	<td><strong>Hazard Symbols</strong></td>
-						    	</tr>
-						    	<tr>
-						      		<td>Sodium (483745)</td>
-						      		<td>Sigma-Aldrich Company Ltd</td>
-						      		<td><img src="assets/media/risk/symbols.png" alt=""></td>
-						    	</tr>
-						    	<tr>
-						      		<td> <a href="#"> Expand <img src="assets/media/risk/zoom_icon.png" alt=""></a></td>
-						    	</tr>
-						  	</tbody>
-                        </table>
-                        
-					</div>
-                </div>
-                <div class="d-flex align-items-center mt-4 mb-4">
-                    <a class="ml-auto mr-5 font-weight-500 font-ash">Save &amp; Previous</a>
-                    <button class="btn btn-custom-blue btn-wide">Save &amp; Next</button>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--end::Substance Accordion-->
+    <!-- begin::Substance Information accordion -->
+    <?php include('accordion-sections/substance_information.php') ?>
+    <!-- end::Substance Information accordion -->
 
     <!--begin::Task Hazards & Controls-->
-    <div class="accordion accordion-light accordion-toggle-arrow mb-4" id="accordionExample2">
-        <div class="card risk_accord">
-            <div class="card-header" id="headingThree2">
-                <div class="card-title collapsed" data-toggle="collapse" data-target="#collapseThree2" aria-expanded="false" aria-controls="collapseOne2">
-                    <span>Not Started</span> Task Hazards & Controls
-                </div>
-            </div>
-            <div id="collapseThree2" class="collapse" aria-labelledby="headingThree2" data-parent="#accordionExample2">
-                <div class="card-body pl-4 pr-4 pt-1 pb-1 haz">
-                   <img src="assets/media/risk/hazard_sect.png" alt="">
-                </div>
-                <div class="d-flex align-items-center mt-4 mb-4">
-                    <a class="ml-auto mr-5 font-weight-500 font-ash">Save &amp; Previous</a>
-                    <button class="btn btn-custom-blue btn-wide">Save &amp; Next</button>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include('accordion-sections/task_hazards.php') ?>
     <!--end::Task Hazards & Controls-->
 </div>
 
